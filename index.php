58aeb376201506477589165ede9d8e8fb056a0f5
ihajaina <ihajaina.nomenjanahary1@gmail.com>
2023-06-07 09:22:23 +0300

    Affichage des artistes dans un tableau

--- index.php
+++ index.php
@@ -17,9 +17,16 @@ $sql = "SELECT * FROM Artist";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+  echo '<table style="border-collapse: collapse; width: 70%;margin:0px auto;">';
+  echo '<tr><th style="border: 1px solid black; padding: 8px;">NUMERO</th><th style="border: 1px solid black; padding: 8px;">LES NOMS DES ARTISTES</th></tr>';
+  
+  $count = 1;
   while($row = $result->fetch_assoc()) {
-    echo "Artist Name: " . $row["Name"] . "<br>";
+    echo '<tr><td style="border: 1px solid black; padding: 8px;">' . $count . '</td><td style="border: 1px solid black; padding: 8px;">' . $row["Name"] . '</td></tr>';
+    $count++;
   }
+  
+  echo '</table>';
 } else {
   echo "Aucun artistes trouves";
 }
