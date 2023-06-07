15b50d2f9a68144a0378c4f94d6b8e2c6969a95a
Naly Rakotomanampy <nalintsoarakotomanampy@gmail.com>
2023-06-07 09:18:50 +0300

    Toute les conventions en html

--- index.php
+++ index.php
@@ -19,8 +19,23 @@ catch(PDOException $e)
 $query=$connexion->prepare("SELECT * FROM Artist");
 $query->execute();
 $result=$query->fetchAll();
-echo "<ul>";
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <ul>
+';
 foreach($result as $coco){
   echo "<li>".$coco['Name']."</li>";
 }
-echo "</ul>";
+echo "  
+  </ul>
+  </body>
+  </html>
+";
