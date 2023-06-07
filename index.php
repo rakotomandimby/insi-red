e2768d48159046f368e40eb75d953ea6051a0a1c
tendry Mamiaritiana <mamiaritianatendry@gmail.com>
2023-06-07 09:12:03 +0200

    liste des Artiste

--- index.php
+++ index.php
@@ -3,14 +3,26 @@ $servername = "vps-2018.rktmb.org";
 $username = "insi";
 $password = "insi";
 $database = "insi";
-$port = 33066
+$port = 33066;
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $database, $port);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+try {
+  $connexion = new PDO("mysql:host=$servername;dbname=insi;port= $port",$username, $password);
+  // set the PDO error mode to exception
+  $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+  }
+catch(PDOException $e)
+  {
+  echo "Connection failed: " . $e->getMessage();
+  }
+
+$requette=$connexion->prepare("SELECT * FROM Artist");
+$requette->execute();
+$query=$requette->fetchAll();
+echo "<ul>";
+foreach($query as $mt){
+echo "<li>".$mt['Name']."</li>";
 }
-echo "Connected successfully";
-
+echo "</ul>";
