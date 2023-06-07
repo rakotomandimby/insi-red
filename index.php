f6c60d5e1852d73aed98fc0817735cfabf06962c
Rakotomandimby Mihamina <mihamina@rktmb.org>
2023-06-07 08:17:43 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+ <?php
+$servername = "vps-2018.rktmb.org";
+$username = "insi";
+$password = "insi";
+$database = "insi";
+$port = 33066
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database, $port);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
