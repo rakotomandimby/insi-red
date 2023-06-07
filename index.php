7513f49ba50ffcf106231b8742ab80a5acea836e
Mihamina <mihamina@rktmb.org>
2023-06-07 09:10:27 +0300

    remise a l'origine

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 $servername = "vps-2018.rktmb.org";
 $username = "root";
 $password = "";
@@ -13,17 +13,5 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dossier</title>
-</head>
-<body>
-    <h1>Bonjour</h1>
-</body>
-</html>
 
+?>
