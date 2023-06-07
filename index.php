eb957bfdb387f0b5a5921f793eeee871ef4785b1
tendry Mamiaritiana <mamiaritianatendry@gmail.com>
2023-06-07 09:22:59 +0200

    rectification

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ $database = "insi";
 $port = 33066;
 
 // Create connection
-
 try {
   $connexion = new PDO("mysql:host=$servername;dbname=insi;port= $port",$username, $password);
   // set the PDO error mode to exception
@@ -18,11 +17,25 @@ catch(PDOException $e)
   echo "Connection failed: " . $e->getMessage();
   }
 
-$requette=$connexion->prepare("SELECT * FROM Artist");
-$requette->execute();
-$query=$requette->fetchAll();
-echo "<ul>";
-foreach($query as $mt){
-echo "<li>".$mt['Name']."</li>";
-}
-echo "</ul>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+    $requette=$connexion->prepare("SELECT * FROM Artist");
+    $requette->execute();
+    $query=$requette->fetchAll();
+    echo "<ul>";
+    foreach($query as $mt){
+    echo "<li>".$mt['Name']."</li>";
+    }
+    echo "</ul>";
+    ?>
+</body>
+</html>
