81b77191cd357fa9d3746fb7ae79d3c396317053
tendry Mamiaritiana <mamiaritianatendry@gmail.com>
2023-06-14 09:19:57 +0200

    rectification

--- code/group07/index.php
+++ code/group07/index.php
@@ -7,7 +7,7 @@ $port = 33066;
 
 // Create connection
 try {
-  $connexion = new PDO("mysql:host=$servername;dbname=insi;port= $port",$username, $password);
+  $connexion = new PDO("mysql:host=$servername;dbname=$database;port= $port", $username, $password);
   // set the PDO error mode to exception
   $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
