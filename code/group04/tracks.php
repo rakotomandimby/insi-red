d07fe2da76b5a2e31e138de6174925135fc5348c
ONJANIRINA NOMANJANAHARY Michel <mihangytsipongo@gmail.com>
2023-06-14 09:57:57 +0300

    Tracks group04

--- code/group04/tracks.php
+++ code/group04/tracks.php
@@ -1,9 +1,9 @@
 <?php
 $servername = "vps-2018.rktmb.org";
-$username = "root";
-$password = "";
+$username = "insi";
+$password = "insi";
 $database = "insi";
-$port = 33306;
+$port = 33066;
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database, $port);
@@ -11,7 +11,21 @@ $conn = new mysqli($servername, $username, $password, $database, $port);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
+ }
+// echo "Connected successfully";
+$sql = "SELECT Name FROM Track";
+$resultat = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($resultat) > 0){
+  echo "<h1>Liste des Tracks : </h1>";
+  echo "<ul>";
+  while($row = mysqli_fetch_assoc($resultat)){
+    echo "<li>".$row['Name']."</li>";
+  }
+  echo "</ul>";
+}else{
+  echo "Accun resultat";
 }
-echo "Connected successfully";
+mysqli_close($conn);
 
 ?>
