6df0332ac558589ae10c69ea4467ff6a7b3951bc
Mihamina <mihamina@rktmb.org>
2023-06-14 07:27:37 +0300

    add groups structure

---
+++ group07/albums.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "vps-2018.rktmb.org";
+$username = "root";
+$password = "";
+$database = "insi";
+$port = 33306;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database, $port);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+?>
