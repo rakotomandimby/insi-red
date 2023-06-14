dd6f9ba6dfb9b04ac5f4ef0c9ef1c6de386060b8
Tommy <>
2023-06-14 08:51:04 +0300

    Listes d'artistes

--- code/group01/artists.php
+++ code/group01/artists.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "vps-2018.rktmb.org";
-$username = "root";
-$password = "";
+$username = "insi";
+$password = "insi";
 $database = "insi";
 $port = 33306;
 
@@ -12,6 +12,31 @@ $conn = new mysqli($servername, $username, $password, $database, $port);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+
+$sql = "SELECT * FROM Artist";
+$resultat = $conn->query($sql);
+
+if ($resultat->num_rows > 0){
+    echo '<ol>';
+
+      while($row = $resultat->fetch_assoc()){
+          echo '<li>'  .$row['Name'] . '</li>';
+        }
+
+    echo '</ol>';
+}
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../../Dossier_Tommy/css/bootstrap.min(1).css">
+</head>
+<body>
+  
+</body>
+</html>
