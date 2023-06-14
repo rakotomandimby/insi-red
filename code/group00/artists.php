7d5229d424f4ea2ba26dea002970546260eb330f
Mihamina <mihamina@rktmb.org>
2023-06-14 09:29:54 +0300

    ArtistId instead of Id

--- code/group00/artists.php
+++ code/group00/artists.php
@@ -12,12 +12,13 @@ if (!$conn) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-$sql = "SELECT Name, Id FROM Artist";
+
+$sql = "SELECT Name, ArtistId FROM Artist";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-	echo $row["Name"] . '   ' . $row["Id"] ; 
+	echo $row["Name"] . '   ' . $row["ArtistId"] ; 
     }
 } else {
     echo "0 results";
