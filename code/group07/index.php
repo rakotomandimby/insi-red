8d13ed60df08f2f24619021d75ba6bd7bb64f95e
tendry Mamiaritiana <mamiaritianatendry@gmail.com>
2023-06-14 09:17:54 +0200

    rectification

--- code/group07/index.php
+++ code/group07/index.php
@@ -3,14 +3,14 @@ $servername = "vps-2018.rktmb.org";
 $username = "insi";
 $password = "insi";
 $database = "insi";
-$port = 33306;
+$port = 33066;
 
 // Create connection
 try {
   $connexion = new PDO("mysql:host=$servername;dbname=insi;port= $port",$username, $password);
   // set the PDO error mode to exception
   $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // echo "Connected successfully";
+  echo "Connected successfully";
   }
 catch(PDOException $e)
   {
