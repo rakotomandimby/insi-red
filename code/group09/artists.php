bc5cb4c71fd21946fa6bd72e72a13f516541a667
Darisse <ravoniaina.darisse1@gmail.com>
2023-06-15 20:10:55 +0300

    modifiaction 15

--- code/group09/artists.php
+++ code/group09/artists.php
@@ -30,7 +30,11 @@ if (mysqli_num_rows($result) > 0) {
         echo '</thead>';
         echo '<tbody>';
               echo '<tr>';
-              while($row = mysqli_fetch_assoc($result)){echo "<td> " . $row["Name"]. "</td>"."<br>";}
+              while($row = mysqli_fetch_assoc($result)){
+                echo "<td> " . $row["Id"]. "</td>";
+                echo "<td> " . $row["Name"]. "</td>";
+                echo "<br>";
+              }
               echo '</br>';
         echo '<tbody>';
 
