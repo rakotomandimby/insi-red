4fa3f19dc133e7cdf7a292f90eeeb125f02e8db9
Darisse <ravoniaina.darisse1@gmail.com>
2023-06-15 19:47:55 +0300

    modifiaction 11

--- code/group09/artists.php
+++ code/group09/artists.php
@@ -16,17 +16,7 @@ if ($conn->connect_error) {
 $sql = "SELECT Name FROM Artist";
 $result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    echo '<h1>Liste des Artistes : </h1>';
-    echo '<ul>';
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<li> " . $row["Name"]. "</li>";
-    }
-    echo '</ul>';
-  } else {
-    echo "0 results";
-}
+
 
 mysqli_close($conn);
 
